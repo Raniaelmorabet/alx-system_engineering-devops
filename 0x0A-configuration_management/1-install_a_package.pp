0451c24610cefed1e55d104ec54bcdcf4f41d4a7
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-08-25 19:56:00 +0200

    installing a package with puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# a manifest that install a package
-package { 'Flask':
-	ensure => '2.1.0',
-	provider => 'pip3',
+# puppet declarative script to install flask from pip3.
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
