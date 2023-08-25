81b4de360304089bba93578b14046916f34c22df
Raniaelmorabet <raniaelmorabet9@gmail.com>
2023-08-25 10:51:55 -0700

    creating a file with puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# a manifest that create a file
+file { '/tmp/school':
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
